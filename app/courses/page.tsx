92422a7f9ab735322651fe36d0d70d0da28367c3
kharnkharn <nigamk@rpi.edu>
2024-02-14 01:00:31 -0500

    added function to add current courses

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,13 +1,52 @@
 "use client";
-import { MouseEventHandler, useState } from "react";
+import { MouseEventHandler, useState, useEffect } from "react";
 import CourseCard from "../components/course/CourseCard";
 import ChevronUp from "@/public/assets/svg/chevron-up.svg?svgr";
 import ChevronDown from "@/public/assets/svg/chevron-down.svg?svgr";
 import { useAppContext } from "../contexts/appContext/AppProvider";
 
+type coursesType = {
+  tag: string;
+  courseCode: string;
+  title: string;
+};
+
 const MyCourses = () => {
   const [courseFilter, setCourseFilter] = useState(0);
   const { courseState } = useAppContext();
+  const [ currentCourses, setCurrentCourses ] = useState<coursesType[]>([]);
+
+
+  const addElementToArray = (newCourse:coursesType) => {
+    /*const newCourse: coursesType = {
+      tag: 'someTag',
+      courseCode: 'someBag',
+      title: 'someLag',
+    };*/
+
+    // Adding the new element to the array
+    setCurrentCourses((prevArray: coursesType[]) => [...prevArray, newCourse]);
+  };
+
+  useEffect(() => {
+    addElementToArray({
+      tag: 'CSCI',
+      courseCode: 'CSCI-2400',
+      title: 'Comp Organization',
+    })
+    
+    addElementToArray({
+      tag: 'MATH',
+      courseCode: 'MATH-1100',
+      title: 'Calculus 1',
+    })
+    
+    addElementToArray({
+      tag: 'CHEM',
+      courseCode: 'CHEM-1100',
+      title: 'Intro to Chemistry',
+    })
+  }, []);
 
   return (
     <>
@@ -40,7 +79,13 @@ const MyCourses = () => {
         </div>
       </section>
       <section className="my-4 grid grid-flow-row gap-y-3">
-        <CourseCard tag={["T"]} courseCode="TEST-3000" title="Test1" />
+        {currentCourses.map((element: coursesType, index: number) => (
+          <li key={index}>
+            <CourseCard tag={[element.tag]} courseCode={element.courseCode} title={element.title} />
+          </li>
+        ))}
+        {/*<CourseCard tag={["T"]} courseCode="TEST-3000" title="Test1" />
+        <CourseCard tag={["CSCI"]} courseCode="CSCI-1200" title="Data Structures" />*/}
       </section>
     </>
   );
